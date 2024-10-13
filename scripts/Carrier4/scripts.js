86a2e4e79c88a9915d4bb9c1ce4a40a4515fcd95
Marc Giestin Louis Cordova <154757927+kazuha001@users.noreply.github.com>
2024-10-13 22:57:10 +0800

    Delete scripts directory

--- scripts/Carrier4/scripts.js
+++
@@ -1,9 +0,0 @@
-function boxl1() {
-    window.location.href = "php/retrieve1.php"
-}
-function boxl2() {
-    window.location.href = "php/retrieve2.php"
-}
-function boxl3() {
-    window.location.href = "php/retrieve3.php"
-}
